9c5fb0fe6a0d473d2d1d93dda70a746847d4912d
lvlin <lvlin241@163.com>
2020-09-27 18:29:56 +0800

    init

---
+++ src/components/test/TestScope.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="example">hi</div>
+</template>
+
+<script>
+export default {
+  name: 'TestScope'
+}
+</script>
+
+<style scoped>
+  .example {
+    color: red;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@comps/HelloWorld'
+import TS from '@comps/test/TestScope'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }, {
+      path: '/',
+      name: 'TS',
+      component: TS
+    }
+  ]
+})
